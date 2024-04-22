591deeeb1b1d836aa7021bb3919d21458e4155ee
Leonardo Ledda <leonardoledda@gmail.com>
2024-04-22 21:35:55 +0200

    todo-frontend: Add header
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { TodoItemComponent } from './components/todo-item/todo-item.component'
 import { TodoListComponent } from './pages/todo-list/todo-list.component'
 import { HttpClientModule } from '@angular/common/http'
 import { TodoSourceService } from './services/todo-source.service'
 import { FormsModule } from '@angular/forms'
+import { HeaderComponent } from './components/header/header.component'
 
 @NgModule({
-  declarations: [AppComponent, TodoItemComponent, TodoListComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, NgbModule],
+  declarations: [AppComponent, TodoItemComponent, TodoListComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [TodoSourceService],
   bootstrap: [AppComponent]
 })
