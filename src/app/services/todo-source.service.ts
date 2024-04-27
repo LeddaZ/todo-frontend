40c5eb52661a2424122febb76ac477b14b90aa90
Leonardo Ledda <leonardoledda@gmail.com>
2024-04-27 02:17:34 +0200

    todo-frontend: Switch from proxy to environments
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- src/app/services/todo-source.service.ts
+++ src/app/services/todo-source.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core'
 import { Todo } from '../entities/todo.entity'
 import { isExpired } from '../utils/is-expired'
 import { Observable } from 'rxjs'
+import { environment } from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -11,18 +12,23 @@ export class TodoSourceService {
   constructor(protected http: HttpClient) {}
 
   fetch(showCompleted: boolean) {
-    return this.http.get<Todo[]>(`/api/todos`, { params: { showCompleted: showCompleted } })
+    return this.http.get<Todo[]>(`${environment.apiUrl}/api/todos`, {
+      params: { showCompleted: showCompleted }
+    })
   }
 
   add(title: string, dueDate: string): Observable<Todo> {
-    return this.http.post<Todo>(`/api/todos`, { title, dueDate })
+    return this.http.post<Todo>(`${environment.apiUrl}/api/todos`, { title, dueDate })
   }
 
   check(id: string, checked: boolean, dueDate: string): Observable<Todo> {
     if (checked) {
-      return this.http.patch<Todo>(`/api/todos/${id}/check`, { completed: checked, expired: false })
+      return this.http.patch<Todo>(`${environment.apiUrl}/api/todos/${id}/check`, {
+        completed: checked,
+        expired: false
+      })
     } else {
-      return this.http.patch<Todo>(`/api/todos/${id}/uncheck`, {
+      return this.http.patch<Todo>(`${environment.apiUrl}/api/todos/${id}/uncheck`, {
         completed: checked,
         expired: isExpired(dueDate)
       })
